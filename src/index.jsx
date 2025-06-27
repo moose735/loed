1385c3a40081a5f8ed5bb64d2415d6b479351dbc
moose735 <132318332+moose735@users.noreply.github.com>
2025-06-27 15:18:27 -0400

    Create index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Import your Tailwind CSS base styles
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
